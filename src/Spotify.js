1341d4a8d4b2fbb9ed8e80717b238735343e0b30
Josimar <rocha_josimar@yahoo.com.br>
2019-07-01 00:43:11 -0300

    Create config spotify

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQBeElTQPj823aqnWsyvkR6n9xUfw8Hx_npKWYT2Q5AQJObBZRLG8Goo6CNh7T5q-E6GhwtP-8WduIO8z2KtlZ3QTdhkKXN0JWkVnU8RUv17bB5CHmrM4nKRZq5qusZXV_xtab7vvaG1g6-Osp6JRqSZfzkpqeKx8kR-xZ6PHywg',
+});
+
+export default spotify;
