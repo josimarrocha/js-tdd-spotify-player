8dcdad5daaa86799e912a5ebb080e3e82bc2f965
Josimar <rocha_josimar@yahoo.com.br>
2019-07-01 00:50:24 -0300

    Create archive albumTracks

---
+++ src/albumTracks.js
@@ -0,0 +1,15 @@
+import convertHumanTime from './convertToHumanTime';
+
+function createMarkup(tracks) {
+  return tracks.map(track => `
+    <div class="music" data-track-preview="${track.preview_url}">
+      <p class="music-number">${track.track_number}</p>
+      <p class="music-title">${track.name}</p>
+      <p class="music-duration">${convertHumanTime(track.duration_ms)}</p>
+    </div>`).join('');
+}
+
+export default function renderAlbumTracks(data, element) {
+  const markup = createMarkup(data);
+  element.innerHTML = markup;
+}
